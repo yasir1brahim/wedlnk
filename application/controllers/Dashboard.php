6fbf8801bad747fb4cba9622432440a056016f5b
Developer 62 <dshah.knysys@gmail.com>
2022-03-25 12:07:01 +0500

    Dashboard Task # 6.7

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -23,11 +23,20 @@ class Dashboard extends CI_Controller {
        $data['qtyActiveAttachedProducts'] = array_sum(array_column($activeAttachedProducts,'qty'));
        // Summarized price for active and attached products
        $sum_price_aa_prods = 0;
+       $userProducts = [];
 
        foreach($activeAttachedProducts as $activeAttachedProduct){
         $sum_price_aa_prods+=$activeAttachedProduct->price*$activeAttachedProduct->qty;
+
+        if(!array_key_exists($activeAttachedProduct->user_id,$userProducts)){
+            $userProducts[$activeAttachedProduct->user_id] = [$activeAttachedProduct->price];
+         } else {
+            $userProducts[$activeAttachedProduct->user_id][] = $activeAttachedProduct->price;
+         }
+
        }
        $data['sum_price_aa_prods'] = $sum_price_aa_prods;
+       $data['user_products'] = $userProducts;
        $data['page'] = 'dashboard/index';
        $this->load->view('template/admin',$data);
     }
