3f4505ec830d17271cdb94e941b331d75da5196a
Developer 62 <dshah.knysys@gmail.com>
2022-04-06 14:30:51 +0500

    Migrations code updated

--- application/migrations/002_products.php
+++ application/migrations/002_products.php
@@ -83,6 +83,6 @@ class Migration_products extends CI_Migration {
 
     public function down()
     {
-        $this->dbforge->drop_table('blog');
+        $this->dbforge->drop_table(TBL_PRODUCTS);
     }
 }
